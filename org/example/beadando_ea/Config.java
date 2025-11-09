c2ab6d3eacd2a5348006032a44811707802606c1
Eagi <161360052+PevaDaniel@users.noreply.github.com>
2025-11-09 09:46:31 +0100

    Add files via upload

---
+++ org/example/beadando_ea/Config.java
@@ -0,0 +1,10 @@
+package org.example.beadando_ea;
+
+import com.oanda.v20.account.AccountID;
+
+public class Config {
+    private Config() {}
+    public static final String URL = "https://api-fxpractice.oanda.com";
+    public static final AccountID ACCOUNTID = new AccountID("101-004-37422618-001");
+    public static final String TOKEN = "d51bc1f93573cca85376ca311bc3e65f-128d7b0bc41c5a6934e75d36426118de";
+}
---
+++ org/example/beadando_ea/MessageActPrice.java
@@ -0,0 +1,13 @@
+package org.example.beadando_ea;
+
+public class MessageActPrice {
+    private String Instrument;
+
+    public String getInstrument() {
+        return Instrument;
+    }
+
+    public void setInstrument(String instrument) {
+        Instrument = instrument;
+    }
+}
